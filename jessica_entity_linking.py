47952503c477ea406fe2bc2ac0459d777b0db261
Jessica Gaoyuan Liang <gaoyuanliang@outlook.com>
2020-10-29 19:15:36 +0400

    Update jessica_entity_linking.py

--- jessica_entity_linking.py
+++ jessica_entity_linking.py
@@ -1,7 +1,32 @@
 ##############jessica_entity_linking.py########
+import rdflib
 import requests
 
+g = rdflib.Graph()
+g.parse("page_ids_en_small.ttl", format='ttl')
+
+def wikipage_id_to_dbpedia_id(entity_id):
+	try:
+		dbpedia_entity_id = [t[0].toPython() for t in 
+			g.query(u"""
+			SELECT ?dbpedia_entity_id
+			WHERE {
+			?dbpedia_entity_id 
+			<http://dbpedia.org/ontology/wikiPageID>
+			"%s"^^<http://www.w3.org/2001/XMLSchema#integer> .
+			} LIMIT 1
+			"""%(entity_id))]
+		dbpedia_entity_id = dbpedia_entity_id[0]
+		return dbpedia_entity_id
+	except:
+		return None
+
+'''
+wikipage_id_to_dbpedia_id("4531823")
+'''
+
 def entity_linking(text):
+	output = []
 	try:
 		r = requests.post("http://localhost:8080/dexter-webapp/api/rest/annotate", 
 			data = {
@@ -9,11 +34,18 @@ def entity_linking(text):
 				"n":50, "wn":False, "debug":False, "format":"text", "min-conf":"0.5",
 			})
 		#print(r.json())
-		return [
-			{'mention':s['mention'],
+		entities = r.json()['spots']
+		for s in entities:
+			output.append({'mention':s['mention'],
 			'entity_wikipage_id':str(s['entity']),
 			'sentence':text,
-			} for s in r.json()['spots']]
+			'entity_dbpedia_id':wikipage_id_to_dbpedia_id(str(s['entity'])),
+			})
+		return output
 	except:
-		return None
+		return output
+
+'''
+entity_linking("I will go to Dubai of the United Arab Emirates.")
+'''
 ##############jessica_entity_linking.py########
