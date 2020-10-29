9534cee077ea19ffefb320208e6e136bace949fc
Jessica Gaoyuan Liang <gaoyuanliang@outlook.com>
2020-10-29 19:16:13 +0400

    Update jessica_entity_linking_example.py

--- jessica_entity_linking_example.py
+++ jessica_entity_linking_example.py
@@ -2,9 +2,15 @@
 
 from jessica_entity_linking import entity_linking
 
-entities = entity_linking("I live at Abu Dhabi but study in Heriot-Watt University Dubai.")
+text = u"the city of Abu Dhabi"
+
+entities = entity_linking(text)
 
 for e in entities:
 	print(e)
 
+'''
+{'entity_wikipage_id': '3956428', 'sentence': 'I live at Abu Dhabi but study in Heriot-Watt University Dubai.', 'mention': 'heriot watt university dubai'}
+{'entity_wikipage_id': '18950756', 'sentence': 'I live at Abu Dhabi but study in Heriot-Watt University Dubai.', 'mention': 'abu dhabi'}
+''' 
 #######jessica_entity_linking_example.py########
